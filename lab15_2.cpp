fb37688b8c5d524a8b4dac96b9977d982a9c990d
gamxu <wuttipat.did@gmail.com>
2022-01-11 17:13:08 +0700

    lab15_2 completed

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,30 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double A[][N]){
+	for(int i=0;i<N;i++){
+		cout << "Row "<< i+1 << ": ";
+		cin >> A[i][0] >> A[i][1] >> A[i][2] >> A[i][3] >> A[i][4];
+	}
+}
+
+void findLocalMax(const double A[][N], bool B[][N]){
+	for(int x=0;x<5;x++){
+		for(int y=0;y<5;y++){
+			int val = A[x][y];
+			if(x==0||y==0||x==4||y==4) B[x][y] = 0;
+			else if(val>=A[x-1][y]&&val>=A[x+1][y]&&val>=A[x][y-1]&&val>=A[x][y+1]) B[x][y] = 1;
+			else B[x][y] = 0;
+		}	
+	}
+}
+
+void showMatrix(const bool B[][N]){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			cout << B[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
